5469581ceb881a0f90794a747e595eef8100b0e1
cyberaom <36754454+cyberaom@users.noreply.github.com>
2018-03-02 11:33:52 +0700

    Add files via upload

---
+++ calTRI.js
@@ -0,0 +1,42 @@
+let myPort = require('./myPortValue.json');
+let backDate = 30 //Period: 30 90 180 360
+const totalDate = myPort.length;
+//console.log(totalDate);
+let TotalReturn=[];
+let TRIBefore = 100;
+var TotalReturnIndex = 100;
+
+//function calculate Total Return
+var TR = (ValueBefore,ValueNow,Order) => ((ValueNow-Order)/ValueBefore)-1;
+// ---------
+
+//function calculate Total Return Index
+var TRI = (TotalReturn) => {
+    TotalReturnIndex = TotalReturnIndex*(1+TotalReturn);
+    //console.log(i,TRIBefore-100);
+    return TotalReturnIndex;
+}
+// --------------
+
+if(backDate<=myPort.length){
+    for(let i=backDate-1;i>=1;i--){
+        TotalReturn[i] = TR(myPort[i].Value,myPort[i-1].Value,myPort[i-1].Order);
+        TotalReturnIndex = TRI(TotalReturn[i]);
+        //console.log(i,TotalReturn[i]);
+        //console.log(i,TRI(TotalReturn[i]));
+    }
+    TotalReturnIndex = TotalReturnIndex-100;
+    TotalReturnIndex = TotalReturnIndex.toFixed(2);
+    console.log('Your Return= '+TotalReturnIndex+'%');
+}else{
+    console.log('Your Investment Period = '+myPort.length+' Days');
+    for(let i=myPort.length-1;i>=1;i--){
+        TotalReturn[i] = TR(myPort[i].Value,myPort[i-1].Value,myPort[i-1].Order);
+        TotalReturnIndex = TRI(TotalReturn[i]);
+        //console.log(i,TotalReturn[i]);
+        //console.log(i,TRI(TotalReturn[i]));
+    }
+    TotalReturnIndex = TotalReturnIndex-100;
+    TotalReturnIndex = TotalReturnIndex.toFixed(2);
+    console.log('Your Return= '+TotalReturnIndex+'%');
+}
